9c18ac78fac0b327d7aabce55d138845fce27d80
n4z03 <149629013+n4z03@users.noreply.github.com>
2026-04-21 14:53:42 -0400

    fixed error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/auth" element={<AuthPage onLogin={setCurrentUser} />} />
     </Routes>
   )
 }
